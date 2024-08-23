3379a7962d1b2d43f8ba73d020f997c38603c608
prince04kumar <princekumar72131@gmail.com>
2024-08-23 10:42:19 +0530

    final

--- script.js
+++ script.js
@@ -1,75 +1,4 @@
-
-
-const carousel = document.querySelector(".carousel");
-const images = document.querySelectorAll(".carousel img");
-const totalImages = images.length;
-let index = 0;
-
-const firstClone = images[0].cloneNode(true);
-carousel.appendChild(firstClone);
-
-function showNextImage() {
-  index++;
-  carousel.style.transition = "transform 1s ease";
-  carousel.style.transform = `translateX(${-index * 100}%)`;
-
-  if (index === totalImages) {
-    setTimeout(() => {
-      carousel.style.transition = "none";
-      carousel.style.transform = `translateX(0)`;
-      index = 0;
-    }, 1000);
-  }
-}
-setInterval(showNextImage, 4000);
-
-
-// const carousel2 = document.querySelector(".carousel2");
-// const images2 = document.querySelectorAll(".carousel2 img");
-// const totalImages2 = images2.length;
-// let index2 = 0;
-
-// const firstClone2 = images2[0].cloneNode(true);
-// carousel2.appendChild(firstClone2);
-
-// function showNextImage2() {
-//   index2++;
-//   carousel2.style.transition = "transform 1s ease";
-//   carousel2.style.transform = `translateX(${-index2 * 100}%)`;
-
-//   if (index2 === totalImages2) {
-//     setTimeout(() => {
-//       carousel2.style.transition = "none";
-//       carousel2.style.transform = `translateX(0)`;
-//       index2 = 0;
-//     }, 1000);
-//   }
-// }
-// setInterval(showNextImage2, 4000);
-
-
-// const carousel3 = document.querySelector(".carousel3");
-// const images3= document.querySelectorAll(".carousel3 img");
-// const totalImages3 = images3.length;
-// let index3 = 0;
-
-// const firstClone3 = images3[0].cloneNode(true);
-// carousel3.appendChild(firstClone3);
-
-// function showNextImage3() {
-//   index3++;
-//   carousel3.style.transition = "transform 1s ease";
-//   carousel3.style.transform = `translateX(${-index3 * 100}%)`;
-
-//   if (index3 === totalImages3) {
-//     setTimeout(() => {
-//       carousel3.style.transition = "none";
-//       carousel3.style.transform = `translateX(0)`;
-//       index3 = 0;
-//     }, 1000);
-//   }
-// }
-// setInterval(showNextImage3, 4000);
+// //for sixes and fores.
 const carousels = document.querySelectorAll(".carousel-containerA");
 carousels.forEach((carousel) => {
 const carouselA = carousel.querySelector(".carouselA");
@@ -93,5 +22,61 @@ function showNextImageA() {
     }, 1000);
   }
 }
-setInterval(showNextImageA, 4000);
+function startCarousel() {
+  motion = setInterval(showNextImageA, 4000);
+}
+
+function stopCarousel() {
+  clearInterval(motion);
+}
+
+startCarousel();
+carouselA.addEventListener('mouseenter', stopCarousel);
+carouselA.addEventListener('mouseleave', startCarousel);
 });
+
+
+//Classes for images
+class Carousel {
+  constructor(selector,innerselector,interval) {
+    this.carousel = document.querySelector(selector);
+    this.images = this.carousel.querySelectorAll(innerselector);
+    this.totalImages = this.images.length;
+    this.index = 0;
+    this.interval = interval;
+
+    const firstClone = this.images[0].cloneNode(true);
+    this.carousel.appendChild(firstClone);
+
+    this.startCarousel();
+    this.carousel.addEventListener('mouseenter', () => this.stopCarousel());
+    this.carousel.addEventListener('mouseleave', () => this.startCarousel());
+  }
+
+  showNextImage() {
+    this.index++;
+    this.carousel.style.transition = "transform 1s ease";
+    this.carousel.style.transform = `translateX(${-this.index * 100}%)`;
+
+    if (this.index === this.totalImages) {
+      setTimeout(() => {
+        this.carousel.style.transition = "none";
+        this.carousel.style.transform = `translateX(0)`;
+        this.index = 0;
+      }, 1000);
+    }
+  }
+
+  startCarousel() {
+    this.intervalId = setInterval(() => this.showNextImage(), this.interval);
+  }
+
+  stopCarousel() {
+    clearInterval(this.intervalId);
+  }
+}
+new Carousel(".carousel","img",5000);
+new Carousel(".carousel2","img",4000);
+new Carousel(".carousel3","img",4000);
+
+

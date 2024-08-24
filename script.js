11426520ad9ae60a9b7ed89af26308b078fab24f
prince04kumar <princekumar72131@gmail.com>
2024-08-24 11:32:38 +0530

    final push

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
 // //for sixes and fores.
 const carousels = document.querySelectorAll(".carousel-containerA");
 carousels.forEach((carousel) => {
-const carouselA = carousel.querySelector(".carouselA");
-const imagesA= carousel.querySelectorAll(".carouselA section");
-const totalImagesA = imagesA.length;
-let indexA = 0;
+  const carouselA = carousel.querySelector(".carouselA");
+  const imagesA = carousel.querySelectorAll(".carouselA section");
+  const totalImagesA = imagesA.length;
+  let indexA = 0;
 
-const firstCloneA = imagesA[0].cloneNode(true);
-carouselA.appendChild(firstCloneA);
+  const firstCloneA = imagesA[0].cloneNode(true);
+  carouselA.appendChild(firstCloneA);
 
-function showNextImageA() {
-  indexA++;
-  carouselA.style.transition = "transform 1s ease";
-  carouselA.style.transform = `translateX(${-indexA * 100}%)`;
+  function showNextImageA() {
+    indexA++;
+    carouselA.style.transition = "transform 1s ease";
+    carouselA.style.transform = `translateX(${-indexA * 100}%)`;
 
-  if (indexA === totalImagesA) {
-    setTimeout(() => {
-      carouselA.style.transition = "none";
-      carouselA.style.transform = `translateX(0)`;
-      indexA = 0;
-    }, 1000);
+    if (indexA === totalImagesA) {
+      setTimeout(() => {
+        carouselA.style.transition = "none";
+        carouselA.style.transform = `translateX(0)`;
+        indexA = 0;
+      }, 1000);
+    }
   }
-}
-function startCarousel() {
-  motion = setInterval(showNextImageA, 4000);
-}
+  function startCarousel() {
+    motion = setInterval(showNextImageA, 4000);
+  }
 
-function stopCarousel() {
-  clearInterval(motion);
-}
+  function stopCarousel() {
+    clearInterval(motion);
+  }
 
-startCarousel();
-carouselA.addEventListener('mouseenter', stopCarousel);
-carouselA.addEventListener('mouseleave', startCarousel);
+  startCarousel();
+  carousel.addEventListener("mouseenter", stopCarousel);
+  carousel.addEventListener("mouseleave", startCarousel);
 });
-
 
 //Classes for images
 class Carousel {
-  constructor(selector,innerselector,interval) {
+  constructor(selector, innerselector, interval) {
     this.carousel = document.querySelector(selector);
     this.images = this.carousel.querySelectorAll(innerselector);
     this.totalImages = this.images.length;
@@ -49,8 +48,8 @@ class Carousel {
     this.carousel.appendChild(firstClone);
 
     this.startCarousel();
-    this.carousel.addEventListener('mouseenter', () => this.stopCarousel());
-    this.carousel.addEventListener('mouseleave', () => this.startCarousel());
+    this.carousel.addEventListener("mouseenter", () => this.stopCarousel());
+    this.carousel.addEventListener("mouseleave", () => this.startCarousel());
   }
 
   showNextImage() {
@@ -75,8 +74,6 @@ class Carousel {
     clearInterval(this.intervalId);
   }
 }
-new Carousel(".carousel","img",5000);
-new Carousel(".carousel2","img",4000);
-new Carousel(".carousel3","img",4000);
-
-
+new Carousel(".carousel", "img", 5000);
+new Carousel(".carousel2", "img", 4000);
+new Carousel(".carousel3", "img", 4000);
